949428caac1e6275cc0d6e9fb7570dfb2edb5e6b
MaksBukharskiy <maksbuharskij16@.com>
2025-04-11 15:58:37 +0500

    Learning how to work with static

---
+++ src/LearningStatic.java
@@ -0,0 +1,17 @@
+public class LearningStatic {
+    public static void main(String[] args) {
+        Human.books = "Harry Potter";
+        Human.howMany = 12;
+
+        Human.total();
+    }
+}
+
+class Human{
+    public static String books;
+    public static int howMany;
+
+    public static void total(){
+        System.out.println("You have read " + howMany + " " + books + " books");
+    }
+        }
