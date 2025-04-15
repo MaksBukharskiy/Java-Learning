0fa60da6f91f191c9b30130b3d2468cdf878c423
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-04-15 21:42:49 +0500

    just some global push

--- src/Polymorphysm/Animal.java
+++ src/Polymorphysm/Animal.java
@@ -4,4 +4,9 @@ public class Animal {
     public void makeSound(){
         System.out.println("Tis is animal");
     }
+
+    public void eat(String food){
+        System.out.println("Animal is eating" + food);
+    }
 }
+ 
---
+++ src/PolymorphysmProgram/Main.java
@@ -0,0 +1,7 @@
+package PolymorphysmProgram;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
