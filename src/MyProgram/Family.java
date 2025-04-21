16410ea442524c3ac552b3ef6e31969440da1d15
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-04-21 15:02:00 +0500

    Re-writed class Family, because classes Max and Arianna implements the interface

--- src/MyProgram/Family.java
+++ src/MyProgram/Family.java
@@ -2,8 +2,8 @@ package MyProgram;
 
 public class Family {
     public static void main(String [] args){
-        Maks maks = new Maks("Maks");
-        Arianna arianna = new Arianna("Arianna");
+        Info maks = new Maks("Maks"); //так как сущ метод из интерфейса Info
+        Info arianna = new Arianna("Arianna"); //так как сущ метод из интерфейса Info
 
         maks.totalInfo();
         arianna.totalInfo();
