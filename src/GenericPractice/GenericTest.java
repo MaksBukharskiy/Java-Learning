198a717e90eb1855a6bd2f942fa95a5405d6650c
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-05-11 22:23:12 +0500

    Learned how to work with generic methods, nice thing, saves more time)))

--- src/GenericPractice/GenericTest.java
+++ src/GenericPractice/GenericTest.java
@@ -5,7 +5,10 @@ public class GenericTest {
         Printer <Integer> printer = new Printer<>(52);
         printer.print();
 
-        Printer<String> printerString= new Printer<>("Something");
+        Printer<String> printerString = new Printer<>("Something");
         printerString.print();
+
+        Printer <Double> printerDouble = new Printer<>(1212.0);
+        printerDouble.print();
     }
 }
