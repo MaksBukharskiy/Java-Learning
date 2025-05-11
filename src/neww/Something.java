526dac2004c6a6d132fdb3083700bfa86e44e256
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-05-11 22:21:41 +0500

    Learned how to work with generic methods, nice thing, saves more time)))

---
+++ src/neww/Something.java
@@ -0,0 +1,8 @@
+package neww;
+
+class Pair {
+    public static <K, V> String createPair(K key, V value){
+        return key + " : " + value;
+    }
+}
+
---
+++ src/neww/Test.java
@@ -0,0 +1,8 @@
+package neww;
+
+public class Test {
+    public static void main(String[] args) {
+        String result1 = Pair.createPair("abc", 123);
+        System.out.println(result1);
+    }
+}
