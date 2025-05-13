6fdc5239e2aac90049672e86fda7bcb9a2b1ca31
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-05-13 17:31:07 +0500

    Jumped to collections(Difference between Array and ArrayList), wrote simple program to understanding this topic

---
+++ src/ArraysListLearning/Test.java
@@ -0,0 +1,13 @@
+package ArraysListLearning;
+import java.util.ArrayList;
+
+public class Test  {
+    public static void main(String[] args) {
+        ArrayList <Integer> newList = new ArrayList<>();
+        newList.add(89);
+        System.out.println(newList);
+
+        newList.remove(0);
+        System.out.println(newList);
+    }
+}
