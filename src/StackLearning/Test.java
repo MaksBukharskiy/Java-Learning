37f5529002fb38e728a9fa72642b313968c54c1b
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-06-03 19:34:06 +0500

    Ended Iterator topic, wrote program With Stack

---
+++ src/StackLearning/Test.java
@@ -0,0 +1,16 @@
+package StackLearning;
+
+import java.util.Stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("a");
+        stack.push("b");
+
+        System.out.println(stack.pop());
+        System.out.println(stack.search("e"));
+        
+    }
+}
