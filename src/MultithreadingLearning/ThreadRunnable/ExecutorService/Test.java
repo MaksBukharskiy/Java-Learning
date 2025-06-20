493ce49ad5e4f452b9c70f67ddbeb41b5857e9cd
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-06-20 18:48:18 +0500

    Ended Collections, went to the new topic, called Multithreading, learned Executor service and how to work with them

---
+++ src/MultithreadingLearning/ThreadRunnable/ExecutorService/Test.java
@@ -0,0 +1,26 @@
+package MultithreadingLearning.ThreadRunnable.ExecutorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for(int i = 0; i < 5; i++){
+            int counter = i;
+
+            executor.execute(() -> {
+                System.out.println("Thread " + Thread.currentThread().getName());
+
+                try{
+                    Thread.sleep(2000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+    }
+}
