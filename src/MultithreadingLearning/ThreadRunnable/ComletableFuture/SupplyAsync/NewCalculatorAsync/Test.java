a36bce54c9fd92e8bcee7da4dedbfa6ba1074784
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-06-25 19:03:07 +0500

    Ended Collections, went to the new topic, called Multithreading,wrote mini program and practice, using supplyAsync(); , runAsync(); ,  thenAccept(); Now i am commiting the main file, file which call all other classes

---
+++ src/MultithreadingLearning/ThreadRunnable/ComletableFuture/SupplyAsync/NewCalculatorAsync/Test.java
@@ -0,0 +1,23 @@
+package MultithreadingLearning.ThreadRunnable.ComletableFuture.SupplyAsync.NewCalculatorAsync;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Test {
+    public static void main(String[] args) {
+
+        CalculatorService calculator = new CalculatorService();
+        LoggerService logger = new LoggerService();
+
+        CompletableFuture<Integer> future1 = calculator.getNumber1();
+        CompletableFuture<Integer> future2 = calculator.getNumber2();
+
+        CompletableFuture<Integer> sumFuture = future1.thenCombine(future2, (a, b) -> a + b);
+        CompletableFuture<Integer> combineFuture = future1.thenCombine(future2, (a, b) -> a * b);
+
+        sumFuture.thenAccept(sum -> System.out.println("Sum: " + sum));
+        combineFuture.thenAccept(resultof -> System.out.println("Result: " + resultof));
+
+        logger.logCompletion().thenRun(() -> System.out.println("Done"));
+
+    }
+}
