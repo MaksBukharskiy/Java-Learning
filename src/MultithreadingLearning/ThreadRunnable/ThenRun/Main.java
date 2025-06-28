5d80faa9fe5e1d4278aa89fd5a912bef3a4d0d5f
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-06-28 21:07:41 +0500

    Ended Collections, went to the new topic, called Multithreading,wrote mini program for learning thenRun() and repeat runAsync()

---
+++ src/MultithreadingLearning/ThreadRunnable/ThenRun/Main.java
@@ -0,0 +1,9 @@
+package MultithreadingLearning.ThreadRunnable.ThenRun;
+
+public class Main {
+    public static void main(String[] args) {
+        ThenAsync.runAsync();
+
+        ThenRun.thenRun();
+    }
+}
