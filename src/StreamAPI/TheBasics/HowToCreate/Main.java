c9dcdd5071b65d8ede07d41b0df29368e110c7b6
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-14 19:38:44 +0500

    feat: add stream filtering examples for strings and integers

---
+++ src/StreamAPI/TheBasics/HowToCreate/Main.java
@@ -0,0 +1,26 @@
+package StreamAPI.TheBasics.HowToCreate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = List.of("Apple", "Banana", "Cherry", "Grape");
+        List<Integer> integerList = List.of(1, 9, 3, 5, 7);
+
+        List<String> filteredList = list.stream()
+                .filter(s -> s.length() > 5).map(String::toLowerCase)
+                .toList();
+
+        List<Integer> filteredInteger = integerList.stream()
+                        .filter(num -> num > 5).map(nums -> nums * nums)
+                                .toList();
+
+        System.out.println(filteredList);
+        System.out.println(filteredInteger);
+
+
+    }
+}
