7c4ef1539e2bbc9b879221ea293acc25d5440a17
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-14 20:22:33 +0500

    feat: worked with (.filter(), .map(), forEach(), list.of() )

--- src/StreamAPI/TheBasics/HowToCreate/Main.java
+++ src/StreamAPI/TheBasics/HowToCreate/Main.java
@@ -10,6 +10,8 @@ public class Main {
         List<String> list = List.of("Apple", "Banana", "Cherry", "Grape");
         List<Integer> integerList = List.of(1, 9, 3, 5, 7);
 
+        List<String> forEachIteration = List.of("hell");
+
         List<String> filteredList = list.stream()
                 .filter(s -> s.length() > 5).map(String::toLowerCase)
                 .toList();
@@ -17,6 +19,10 @@ public class Main {
         List<Integer> filteredInteger = integerList.stream()
                         .filter(num -> num > 5).map(nums -> nums * nums)
                                 .toList();
+
+        forEachIteration.stream()
+                .map(foreach -> foreach + 'o')
+                .forEach(System.out::println);
 
         System.out.println(filteredList);
         System.out.println(filteredInteger);
