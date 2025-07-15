52eac8ac8de93afe016c306f9bf0c39393659a24
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-15 23:48:35 +0500

    feat: Demonstrated stream processing by removing duplicates, reversing order, adding suffixes and filtering names by length

--- src/StreamAPI/TheBasics/HowToCreate/secondDayProgram/EditingTheList.java
+++ src/StreamAPI/TheBasics/HowToCreate/secondDayProgram/EditingTheList.java
@@ -1,4 +1,21 @@
 package StreamAPI.TheBasics.HowToCreate.secondDayProgram;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class EditingTheList {
+
+    public List<String> filterByTheDuplicates(List<String> nameInput){
+        return nameInput.stream()
+                .distinct().toList();
+    }
+
+    public List<String> filterByTheLength(List<String> list, Integer length){
+        return list.stream().filter(filterI -> filterI.length() >= length).toList();
+    }
+
+    public List<String> reverseListAndAdd(List<String> reverse){
+        return reverse.stream().sorted(Comparator.reverseOrder()).map(res -> res + "d").toList();
+    }
+
 }
