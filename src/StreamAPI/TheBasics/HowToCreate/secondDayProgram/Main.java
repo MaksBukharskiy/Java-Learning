6038d4d97d8f47954ccc8e9f2c33344edf771e23
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-15 23:47:26 +0500

    feat: Added three stream operations: distinct names, length-based filtering, and reversed sorted list with 'd' suffix transformation

--- src/StreamAPI/TheBasics/HowToCreate/secondDayProgram/Main.java
+++ src/StreamAPI/TheBasics/HowToCreate/secondDayProgram/Main.java
@@ -1,4 +1,27 @@
 package StreamAPI.TheBasics.HowToCreate.secondDayProgram;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        EditingTheList theList = new EditingTheList();
+        List<String> list = new ArrayList<>();
+
+        list.add("Maks");
+        list.add("Mak");
+        list.add("M");
+        list.add("Maks");
+        list.add("Ma");
+
+        List<String> resultNumberOne = theList.filterByTheDuplicates(list);
+        List<String> resultNumberTwo =theList.reverseListAndAdd(list);
+        List<String> resultNumberThree =theList.filterByTheLength(list, 3);
+
+        System.out.println(resultNumberOne);
+        System.out.println(resultNumberTwo);
+        System.out.println(resultNumberThree);
+
+    }
 }
