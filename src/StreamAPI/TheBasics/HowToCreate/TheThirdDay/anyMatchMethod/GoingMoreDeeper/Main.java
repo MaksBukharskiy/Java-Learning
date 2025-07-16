c70c564b28195775f9bfa6fc9f18529b8b8d55f4
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-16 16:02:14 +0500

    feat: mplement Stream API validations for users, learned .allMatch() method, how to work with with it

--- src/StreamAPI/TheBasics/HowToCreate/TheThirdDay/anyMatchMethod/GoingMoreDeeper/Main.java
+++ src/StreamAPI/TheBasics/HowToCreate/TheThirdDay/anyMatchMethod/GoingMoreDeeper/Main.java
@@ -8,19 +8,25 @@ public class Main {
         List<User> users = List.of(
                 new User("Maks", 161.0),
                 new User("Maks", 165.0),
-                new User("Artem", 150.0)
+                new User("Artem", 150.1)
         );
 
         boolean isExpensive = users.stream()
                 .filter(userName -> userName.name().contains("M"))
                 .anyMatch(userPrice -> userPrice.price() > 160);
 
-        if(isExpensive){
+        boolean isAllExpensive = users.stream()
+                .allMatch(n -> n.price() > 150);
+
+
+        if(isExpensive && isAllExpensive){
             System.out.println(true);
         }
         else {
             System.out.println(false);
         }
+
+
 
     }
 }
