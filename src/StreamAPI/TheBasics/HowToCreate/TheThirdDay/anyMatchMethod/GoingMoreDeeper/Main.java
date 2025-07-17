395f4e9bd95832f8a99ef776265426a5063acd2b
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-17 16:15:20 +0500

    feat: learned the base of .reduce(), how to work with it and what is it

--- src/StreamAPI/TheBasics/HowToCreate/TheThirdDay/anyMatchMethod/GoingMoreDeeper/Main.java
+++ src/StreamAPI/TheBasics/HowToCreate/TheThirdDay/anyMatchMethod/GoingMoreDeeper/Main.java
@@ -2,7 +2,9 @@ package StreamAPI.TheBasics.HowToCreate.TheThirdDay.anyMatchMethod.GoingMoreDeep
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +16,8 @@ public class Main {
                 new User("Artem", 150.1)
         );
 
+        List<Integer> reduceLearning = List.of(1, 2, 3, 4, 5, 6);
+
         boolean isExpensive = users.stream()
                 .filter(userName -> userName.name().contains("M"))
                 .anyMatch(userPrice -> userPrice.price() > 160);
@@ -24,6 +28,15 @@ public class Main {
         boolean isNoneMatch = users.stream()
                 .noneMatch(p -> p.name().contains("u"));
 
+        int sumReduce = reduceLearning.stream()
+                .sorted(Comparator.reverseOrder())
+                .reduce(0, (a, b) -> a + b);
+
+        Optional<Integer> max = reduceLearning.stream()
+                .reduce(Integer::max);
+
+        System.out.println("Sum of reduce is: " + sumReduce);
+        System.out.println("The max integer is: " + max);
 
         if (isNoneMatch){
             List<User> maksRes = users.stream()
