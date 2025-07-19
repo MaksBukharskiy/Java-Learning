11877e19b6dfee1a196e3b25e1664f58313bf890
MaksBukharskiy <maksbuharskij16@gmail.com>
2025-07-19 19:43:39 +0500

    Implement async password validation logic using CompletableFuture

--- src/StreamAPI/TheBasics/HowToCreate/TheFifthDay/Homework/ListService.java
+++ src/StreamAPI/TheBasics/HowToCreate/TheFifthDay/Homework/ListService.java
@@ -1,4 +1,38 @@
 package StreamAPI.TheBasics.HowToCreate.TheFifthDay.Homework;
 
-public class Comparing {
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ListService {
+    StreamRealization realization = new StreamRealization();
+
+    public CompletableFuture<Boolean> processInfoClient(String userInput) {
+        return CompletableFuture.supplyAsync(() -> {
+            List<PersonNames> clientInfo = List.of(
+                    new PersonNames(List.of("something@maks")),
+                    new PersonNames(List.of("something@artem")),
+                    new PersonNames(List.of("something@maks")),
+                    new PersonNames(List.of("something@elia")),
+                    new PersonNames(List.of("something@alice")),
+                    new PersonNames(List.of("something@alice")),
+                    new PersonNames(List.of("something@joan")),
+                    new PersonNames(List.of("something@adiolla")),
+                    new PersonNames(List.of("something@alina"))
+            );
+
+            List<String> allEmails = realization.puttingIntoTheStream(clientInfo);
+            List<String> domains = realization.filteredList(allEmails);
+            List<String> filteredDomains = realization.upperCaseFilteredList(domains);
+            List<String> uniqueDomains = realization.duplicatesFilteredList(filteredDomains);
+            List<String> finalList = realization.finalSortedList(uniqueDomains);
+
+            System.out.println("finalList: " + finalList);
+
+            if (!finalList.isEmpty()) {
+                String correctPassword = finalList.get(0);
+                return correctPassword.equalsIgnoreCase(userInput);
+            }
+            return false;
+        });
+    }
 }
